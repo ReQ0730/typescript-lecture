393d408dcf267f371cd5975d0c1e9844bc9f0dd2
sawa0730 <sawadataiga0730@gmail.com>
2026-06-30 21:00:10 +0900

    feat: 基底クラス Character を拡張 (課題1)

--- src/mage.ts
+++ src/mage.ts
@@ -1,7 +1,7 @@
 import { Character } from './character.ts';
 
 export class Mage extends Character {
-  override attack() {
+  override attack(_opponent: Character) {
     console.log(`${this.name}は魔法を唱えた！`);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
 import { Mage } from "./mage";
 import { Warrior } from "./warrior";
+import { Enemy } from "./enemy";
 
 // const character = new Character("太郎", 200); // abstract class はインスタンス化出来ないのでエラーが出る
 
 const warrior = new Warrior("アーサー", 100, "エクスカリバー");
-warrior.showStatus();
-warrior.attack();
-
 const mage = new Mage("メディア", 80);
-mage.showStatus();
-mage.attack();
-
-import { Enemy } from "./enemy";
-
 const slime = new Enemy("スライム", 50);
+
+warrior.showStatus();
+warrior.attack(slime); // 引数に slime を渡す
+
+// メイジのターン（スライムを攻撃）
+mage.showStatus();
+mage.attack(slime); // 引数に slime を渡す
+
+// スライムのターン
 slime.showStatus();
-slime.attack();
+slime.attack(warrior); // 引数に warrior を渡す（または mage）
+
 slime.takeDamage(40);
 slime.showStatus();
-slime.attack();
+
+slime.attack(mage); // 引数に mage を渡す（または warrior）
